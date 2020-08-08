93e7cad7a15f7e0eb59e0d86151c7db0efed85c0
Hassan Memon <hassanlakhani3@gmail.com>
2020-08-08 13:54:40 -0700

    Basic Website

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='text-center footer d-flex justify-content-center flex-column'>
+          &copy; copyright lab-8 2020
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import web from '../src/images/img2.svg';
+import Common from './Common';
+
+const Home = () => {
+  return (
+    <>
+      <Common
+        name='Grow Your Business With'
+        imgsrc={web}
+        visit='/service'
+        btnname='Get Started'
+      />
+    </>
+  );
+};
+
+export default Home;
